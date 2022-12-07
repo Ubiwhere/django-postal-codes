a9469d7d665b39990f45acf39640b485ef66f9a0
Diogo Silva <49190578+diogosilva30@users.noreply.github.com>
2022-12-07 15:54:22 +0000

    Update serializers.py

--- django_postal_codes/api/serializers.py
+++ django_postal_codes/api/serializers.py
@@ -242,7 +242,9 @@ class DetailPostalCodeSerializer(PostalCodeSerializer):
     """
     Serializer class for a `PostalCode` in Detail view.
     """
-
+    # Explicitly add id
+    id = serializers.ReadOnlyField(source="id")
+    
     # Upstream fields
     country = serializers.ReadOnlyField(source="locality.county.district.country.name")
     district = serializers.ReadOnlyField(source="locality.county.district.name")
