86d01b84d1eb5216f624ca56364feeb0ee028af6
Diogo Silva <diogosilv30@gmail.com>
2022-12-07 14:38:16 +0000

    Fix Fixture Loading

--- django_postal_codes/management/commands/import_postal_codes.py
+++ django_postal_codes/management/commands/import_postal_codes.py
@@ -2,7 +2,7 @@
 Custom Django command to import data from a GitHub repository
 into the local database
 """
-import shutil
+import os
 import runpy
 from django_postal_codes import BASE_DIR
 from pathlib import Path
@@ -11,6 +11,7 @@ from django.core.management import call_command
 from django.conf import settings
 from django.core.management.base import BaseCommand
 import glob
+import subprocess
 
 # Check if user configured which countries data should be imported
 COUNTRIES_TO_IMPORT = getattr(settings, "DJANGO_POSTAL_CODES_COUNTRIES", None)
@@ -22,19 +23,18 @@ def load_country_fixture(fixture_folder: str) -> None:
     parts. The json is splitted so each part is under 100mb and can be added to source control.
     To load fixture into django we need to glue the file back together
     """
-    file_parts = [
-        path
-        for path in glob.glob(fixture_folder + "/*.json")
-        if not Path(path).stem.startswith("MERGED_")
-    ]
     country_name: str = Path(fixture_folder).stem
     final_fixture_path: str = (
         f"{BASE_DIR}/fixtures/{country_name}/MERGED_{country_name}.json"
     )
-    with open(final_fixture_path, "wb") as wfd:
-        for f in file_parts:
-            with open(f, "rb") as fd:
-                shutil.copyfileobj(fd, wfd)
+
+    if os.path.exists(final_fixture_path):
+        os.remove(final_fixture_path)
+    with open(final_fixture_path, "w") as file:
+        # Call is needed to block execution until script ends
+        # otherwise django will start reading from file and it is not
+        # fully merged yet, resulting in error
+        subprocess.call(f"cat {fixture_folder}/*.json", stdout=file, shell=True)
 
     # Now time to load into django
     # Use a single transaction to speed up loading
